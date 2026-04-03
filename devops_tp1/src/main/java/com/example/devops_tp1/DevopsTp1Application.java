9e9f170f5ecb8063dc2fdfc76bb3adad42661754
Oscar Lin <95445318+Flappyxx@users.noreply.github.com>
2026-04-03 14:42:07 +0200

    add my service

---
+++ devops_tp1/src/main/java/com/example/devops_tp1/DevopsTp1Application.java
@@ -0,0 +1,13 @@
+package com.example.devops_tp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsTp1Application.class, args);
+	}
+
+}
